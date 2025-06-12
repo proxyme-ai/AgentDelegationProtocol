d00b4b1060fba1e2f185b6755a3958981c355c9c
Abhishek M Shivalingaiah <abhishekms1047@gmail.com>
2025-06-12 15:35:01 +0530

    Add dynamic sequence diagram to frontend

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -6,7 +6,9 @@ from tests.utils import start_server
 def test_frontend_demo():
     server = start_server(demo_frontend.app, port=7000)
     resp = requests.get('http://localhost:7000/')
+    flow = requests.get('http://localhost:7000/flow')
     server.shutdown()
     assert resp.status_code == 200
     assert 'Delegation Protocol Demo' in resp.text
-    assert 'alice' in resp.text
+    assert flow.status_code == 200
+    assert 'alice' in flow.text
